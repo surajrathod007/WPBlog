59ca2f972595e3418334ba5ad5072aee494978c4
Suraj <surajsinhrathod75@gmail.com>
2022-01-06 09:01:30 +0530

    S_Branch Commit 2

--- app/src/main/java/com/surajrathod/wpblog/MainActivity.kt
+++ app/src/main/java/com/surajrathod/wpblog/MainActivity.kt
@@ -3,6 +3,7 @@ package com.surajrathod.wpblog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+//S Branch : Author : Suraj
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
