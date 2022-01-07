dd28470a607672c4ce3634d7c247c95056ef5761
Manshal <manshalkhatri51@gmail.com>
2022-01-07 17:51:47 +0530

    minor : layouts designing done

--- app/src/main/java/com/surajrathod/wpblog/fragments/DashboardFragment.kt
+++ app/src/main/java/com/surajrathod/wpblog/fragments/DashboardFragment.kt
@@ -19,14 +19,11 @@ private const val ARG_PARAM2 = "param2"
  */
 class DashboardFragment : Fragment() {
     // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+
         }
     }
 
@@ -38,23 +35,9 @@ class DashboardFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_dashboard, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment DashboardFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            DashboardFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
     }
+
+
 }
