507012bcf84d3e8cbbe68802640463bc1b2118ea
Suraj <surajsinhrathod75@gmail.com>
2022-01-07 21:53:08 +0530

    minor : Added Primary key in PostDao (i forgot that so)

--- app/src/main/java/com/surajrathod/wpblog/Database/PostEntity.kt
+++ app/src/main/java/com/surajrathod/wpblog/Database/PostEntity.kt
@@ -1,10 +1,12 @@
 package com.surajrathod.wpblog.Database
 
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 @Entity(tableName = "PostTable")
 data class PostEntity(
 
+    @PrimaryKey
     var postId : Int,
     var postTitle : String,
     var postImg : String,
