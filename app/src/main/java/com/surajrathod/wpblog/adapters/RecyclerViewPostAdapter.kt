bbf040b6d788a5856cc37b315355f746995c411d
Manshal <manshalkhatri51@gmail.com>
2022-01-07 20:15:47 +0530

    minor : adapter for DashboardFragment.kt done

--- app/src/main/java/com/surajrathod/wpblog/adapters/RecyclerViewPostAdapter.kt
+++ app/src/main/java/com/surajrathod/wpblog/adapters/RecyclerViewPostAdapter.kt
@@ -1,4 +1,34 @@
 package com.surajrathod.wpblog.adapters
 
-class RecyclerViewPostAdapter {
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.surajrathod.wpblog.R
+import com.surajrathod.wpblog.databinding.PostViewHolderBinding
+import com.surajrathod.wpblog.model.PostDetails
+
+class RecyclerViewPostAdapter (val postList : ArrayList<PostDetails>) : RecyclerView.Adapter<RecyclerViewPostAdapter.ViewHolder>(){
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+
+        return ViewHolder(
+                PostViewHolderBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        )
+    }
+
+    override fun getItemCount(): Int {
+        return postList.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val post = postList[position]
+        holder.binding.title.text=post.title
+//        holder.binding.poster.setImageResource()
+        holder.binding.date.text=post.date
+        holder.binding.category.text=post.category
+    }
+
+    inner class ViewHolder(val binding : PostViewHolderBinding) : RecyclerView.ViewHolder(binding.root)
 }
--- app/src/main/java/com/surajrathod/wpblog/fragments/DashboardFragment.kt
+++ app/src/main/java/com/surajrathod/wpblog/fragments/DashboardFragment.kt
@@ -1,16 +1,29 @@
 package com.surajrathod.wpblog.fragments
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.cardview.widget.CardView
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.surajrathod.wpblog.R
+import com.surajrathod.wpblog.adapters.RecyclerViewPostAdapter
+import com.surajrathod.wpblog.databinding.FragmentDashboardBinding
+import com.surajrathod.wpblog.model.PostDetails
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
+lateinit var category0 : CardView
+
+
 
 /**
  * A simple [Fragment] subclass.
@@ -18,7 +31,13 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class DashboardFragment : Fragment() {
-    // TODO: Rename and change types of parameters
+
+
+    val postList = arrayListOf<PostDetails>(
+        PostDetails(1,"title1","img1","1-1-22","tech","desc","url1"),
+        PostDetails(2,"title2","img1","1-1-23","tech","desc","url1"),
+        PostDetails(3,"title3","img1","4-1-24","sports","desc","url1")
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,6 +56,19 @@ class DashboardFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 
+        val recyclerView = view.findViewById<View>(R.id.postList)
+        category0=view.findViewById(R.id.category0)
+
+        if (recyclerView is RecyclerView) {
+            with(recyclerView) {
+                layoutManager = LinearLayoutManager(context)
+                adapter = RecyclerViewPostAdapter(postList)
+            }
+        }
+
+        category0.setOnClickListener {
+            findNavController().navigate(R.id.action_dashboardFragment_to_genericPostsFragment)
+        }
     }
 
 
