3639bba233fa3b0348d806bd1466349e9f7709e9
Suraj <surajsinhrathod75@gmail.com>
2022-01-10 18:34:57 +0530

    major :

--- app/src/main/java/com/surajrathod/wpblog/fragments/DescriptionFragment.kt
+++ app/src/main/java/com/surajrathod/wpblog/fragments/DescriptionFragment.kt
@@ -10,16 +10,25 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.navigation.NavArgs
 import androidx.navigation.fragment.navArgs
+import com.surajrathod.wpblog.Database.PostEntity
 
 
 import com.surajrathod.wpblog.R
+import com.surajrathod.wpblog.databinding.FragmentDescriptionBinding
 
 
 class DescriptionFragment : Fragment() {
 
 
 
-lateinit var postTitle : TextView
+    val args by navArgs<DescriptionFragmentArgs>()
+
+    lateinit var binding: FragmentDescriptionBinding
+    lateinit var favPost : PostEntity
+
+
+
+    lateinit var postTitle : TextView
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -27,6 +36,20 @@ lateinit var postTitle : TextView
         // Inflate the layout for this fragment
 
         val view = inflater.inflate(R.layout.fragment_description, container, false)
+
+        binding = FragmentDescriptionBinding.bind(view)
+
+        //showing views
+
+        binding.apply {
+
+            txtPostTitle.text = args.postObject.title
+            txtPostDate.text = args.postObject.date
+
+        }
+
+
+
 
 
         return view
